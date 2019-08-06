df2db462533432a2992c732bd5546af8aa33808a
Payal Nayee <PNayee@spartaglobal.com>
2019-08-06 22:02:12 +0100

    completed task and tests

--- Raindrops_task_main.py
+++ Raindrops_task_main.py
@@ -3,16 +3,15 @@
 
 
 class Raindrops():
-    def raindrop (n):
-        input("Please enter a number of your choice: ")
 
-for n in range(1,120):
-        if n % 3 == 0 and n% 5 == 0:
-            print("Pling")
-        elif n % 5 == 0:
-            print("Plang")
-        elif n % 7 == 0:
-            print ("Plong")
+    def raindrop (num):
+        for num in range(1,120):
+            if num % 3 == 0 and num% 5 == 0:
+                print("Pling")
+            elif num % 5 == 0:
+                print("Plang")
+            elif num % 7 == 0:
+                print ("Plong")
 
         else:
-            print(n)
+            print(num)
--- Raindrops_task_test.py
+++ Raindrops_task_test.py
@@ -1,24 +1,50 @@
 from Raindrops_task_main import Raindrops
 
-class RaindropsTest():
-
+class RaindropsTest_pytest():
     def test1(self):
         self.assertEqual(Raindrops(1),"1")
 
-    def test1(self):
-        self.assertEqual(Raindrops(3),"Pling")
+    def test2(self):
+        self.assertEqual(Raindrops(74),"Pling")
 
-    def test1(self):
+    def test3(self):
         self.assertEqual(Raindrops(5), "Plang")
 
-    def test1(self):
+    def test4(self):
         self.assertEqual(Raindrops(7), "Plong")
 
-    def test1(self):
-        self.assertEqual(Raindrops(7), "Plong")
+    def test5(self):
+        self.assertEqual(Raindrops(6), "Pling")
 
-    def test1(self):
-        self.assertEqual(Raindrops(7), "Plong")
+    def test6(self):
+        self.assertEqual(Raindrops(9), "Pling")
 
-    def test1(self):
-        self.assertEqual(Raindrops(7), "Plong")
+    def test7(self):
+        self.assertEqual(Raindrops(10), "Plang")
+
+    def test8(self):
+        self.assertEqual(Raindrops(14), "Plong")
+
+    def test9(self):
+        self.assertEqual(Raindrops(15), "PlingPlang")
+
+    def test10(self):
+        self.assertEqual(Raindrops(21), "PlingPlong")
+
+    def test11(self):
+        self.assertEqual(Raindrops(25), "Plang")
+
+    def test12(self):
+        self.assertEqual(Raindrops(35), "PlangPlong")
+
+    def test13(self):
+        self.assertEqual(Raindrops(49), "Plong")
+
+    def test14(self):
+        self.assertEqual(Raindrops(52), "52")
+
+    def test15(self):
+        self.assertEqual(Raindrops(105), "PlingPlangPlong")
+
+    def test16(self):
+        self.assertEqual(Raindrops(500000),"500")
